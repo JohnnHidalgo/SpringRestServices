e1fd2bd85796bb6c48b44959f8dd67bd8b7c5bc1
JohnnHidalgo <31965496+JohnnHidalgo@users.noreply.github.com>
2019-09-14 11:15:19 -0400

    Add constructor in TeacherNotFoundAdvice.java

--- src/main/java/platform/TeacherNotFoundAdvice.java
+++ src/main/java/platform/TeacherNotFoundAdvice.java
@@ -1,4 +1,18 @@
 package platform;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
 public class TeacherNotFoundAdvice {
+
+    @ResponseBody
+    @ExceptionHandler(TeacherNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    String teacherNotFoundHandler(TeacherNotFoundException ex){
+        return ex.getMessage();
+    }
 }
