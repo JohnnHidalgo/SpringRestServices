96bcba77bc8ba0b8e3d0639ba90fbd51033b1cbe
JohnnHidalgo <31965496+JohnnHidalgo@users.noreply.github.com>
2019-09-13 22:15:03 -0400

    Add initDataBAse funtion in LoadDatabase.java

--- src/main/java/platform/LoadDatabase.java
+++ src/main/java/platform/LoadDatabase.java
@@ -1,4 +1,20 @@
 package platform;
 
-public class LoadDatabase {
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Slf4j
+class LoadDatabase {
+
+    @Bean
+    CommandLineRunner initDatabase(TeacherRepository repository) {
+        return args -> {
+            log.info("Preloading " + repository.save(new Teacher("Bilbo Baggins", "burglar")));
+            log.info("Preloading " + repository.save(new Teacher(s"Frodo Baggins", "thief")));
+        };
+    }
 }
