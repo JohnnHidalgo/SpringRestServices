d55b30aedd2966b1c9215df519e96f2577129854
JohnnHidalgo <31965496+JohnnHidalgo@users.noreply.github.com>
2019-09-13 20:43:38 -0400

    Create attributes and constructors for Teacher.java

--- src/main/java/platform/Teacher.java
+++ src/main/java/platform/Teacher.java
@@ -1,4 +1,22 @@
 package platform;
+import lombok.Data;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data
+@Entity
 public class Teacher {
+    private @Id @GeneratedValue Long id;
+    private String name;
+    private String course;
+
+    public Teacher() {
+    }
+
+    public Teacher(String name, String course) {
+        this.name = name;
+        this.course = course;
+    }
 }
